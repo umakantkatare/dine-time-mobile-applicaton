e315462f7a323727882d3e1a1dd1eaa80c3e9bb7
umakant katare <umakantkatare11@gmail.com>
2026-07-26 20:54:16 +0530

    configure tailwindcss

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
 import { Text, View } from "react-native";
-
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello from Dine Time!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Dine-time!
+      </Text>
     </View>
   );
 }
